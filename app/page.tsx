fd5c98c41459ab2e53ec24c67d28d64b5745942a
issac <issac.rajan@gmail.com>
2024-06-07 21:42:25 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -22,54 +22,104 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useState } from "react";
+import { MouseEventHandler, useRef, useState } from "react";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { useForm } from "react-hook-form"
+
+const LoginFormSchema = z.object({
+  UserType: z.string(),
+  UserId: z.string(),
+  UserPwd: z.string()
+});
+import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export default function LoginForm() {
+  const selectVal = useRef();
   const [loginAs, setLoginAs] = useState("Student")
 
-  const LoginAsChanged = (e: string) =>{
+  const LoginAsChanged = (e: string) => {
     setLoginAs(e);
   }
-  
+
+  const form = useForm<z.infer<typeof LoginFormSchema>>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      UserType: "Mentor"
+    }
+  })
+
+  const handleBtnClick = (e: any) => {
+    const values = form.getValues();
+    const userType = values["UserType"];
+    console.log(userType);
+    // e.preventDefault();
+    
+    
+  }
+
   return (
-    <div className="flex justify-center">
-      <Card className="w-4/12 max-w-sm">
-        <CardHeader>
-          <div className="flex gap-4">
-            <Select defaultValue='Student' onValueChange={LoginAsChanged}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue  placeholder="Login as..." />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="Student">Student</SelectItem>
-                  <SelectItem value="Mentor">Mentor</SelectItem>
-                  <SelectItem value="HOD">HOD</SelectItem>
-                  <SelectItem value="Admin">Admin</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <div className="text-2xl ">Login
-            </div>
-          </div>
-          <CardDescription className="mt-16">
-            Enter {loginAs} email below to login to your account.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="test@nmit.ac.in" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Log in</Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Form {...form}>
+      <form>
+        <div className="flex justify-center">
+          <Card className="w-4/12 max-w-sm">
+            <CardHeader>
+              <div className="flex gap-4">
+                <FormField
+                  control={form.control}
+                  name="UserType"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Select name="UserType" onValueChange={field.onChange} defaultValue={field.value}>
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Login as..." />
+                        </SelectTrigger>
+                        <SelectContent >
+                          <SelectGroup >
+                            <SelectItem value="Student">Student</SelectItem>
+                            <SelectItem value="Mentor">Mentor</SelectItem>
+                            <SelectItem value="HOD">HOD</SelectItem>
+                            <SelectItem value="Admin">Admin</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+                <div className="text-2xl ">Login
+                </div>
+              </div>
+              <CardDescription className="mt-16">
+                Enter {loginAs} email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" name="UserId" placeholder="test@nmit.ac.in" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" name="UserPwd" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={handleBtnClick} className="w-full">Log in</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </form>
+    </Form>
   )
 }
--- app/student/semestermark/page.tsx
+++ app/student/semestermark/page.tsx
@@ -33,19 +33,86 @@ import {
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
 export default function HomePage() {
+
+  const invoices = [
+    {
+      invoice: "01",
+      subjectTitle: "Maths",
+      subjectCode: "M01",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "02",
+      subjectTitle: "Python",
+      subjectCode: "P01",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "03",
+      subjectTitle: "Designs",
+      subjectCode: "D01",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "04",
+      subjectTitle: "Java",
+      subjectCode: "J01",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "05",
+      subjectTitle: "React JS",
+      subjectCode: "R01",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "06",
+      subjectTitle: "Node JS",
+      subjectCode: "N01",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "07",
+      subjectTitle: "Database",
+      subjectCode: "DB01",
+      paymentMethod: "Credit Card",
+    },
+  ]
+
   return (
     <div className="flex flex-col min-h-screen w-full">
       <div className="flex justify-between h-full max-h-screen border-b gap-10">
         <div className="flex h-14 items-center  px-4 ">
           <Link href="/" className="flex items-center gap-2 font-semibold">
             <Image
-            src="/header_logo.jpeg"
-            width={100}
-            height={60}
-            className="hidden md:block"
-            alt="NMIT"
-          />
+              src="/header_logo.jpeg"
+              width={100}
+              height={60}
+              className="hidden md:block"
+              alt="NMIT"
+            />
             <span className="text-2xl">CSE Department</span>
           </Link>
         </div>
@@ -90,7 +157,7 @@ export default function HomePage() {
               Sem. Marks
             </Link>
             <Link
-               href="/student/meeting"
+              href="/student/meeting"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-primary transition-all hover:text-primary"
             >
               <Package className="h-4 w-4" />
@@ -100,132 +167,126 @@ export default function HomePage() {
           </nav>
         </div>
         <div className="w-full">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-lg">Student Info</h1>
+          <div className="flex items-center gap-4">
+            <h1 className="text-lg font-semibold md:text-lg">Select Semester</h1>
+            <Select name="SemesterCode" >
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent >
+                <SelectGroup >
+                  <SelectItem value="S3">Semester 3</SelectItem>
+                  <SelectItem value="S4">Semester 4</SelectItem>
+                  <SelectItem value="S5">Semester 5</SelectItem>
+                  <SelectItem value="S6">Semester 6</SelectItem>
+                  <SelectItem value="S7">Semester 7</SelectItem>
+                  <SelectItem value="S8">Semester 8</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
-          <div
-            className="flex rounded-lg border gap-8 p-4 mr-6 shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Full Name
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Name 1
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                USN
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                12345678
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Date of birth
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                10, Jan 2000
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Gender
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Male
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                CET / COMEDK Rank
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                5000 (CET)
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Cagegory of Adminission
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                CET
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Accommodation
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Day scholar
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Mobile
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                +91 123456788
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Email
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                student1@example.com
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-lg">Parent's Info</h1>
-          </div>
-          <div
-            className="flex rounded-lg border gap-8 p-4 mr-6 shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Father Name
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Name2
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Mother Name
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Name22
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Occupation
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                teacher
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Present Address
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Bangalore
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <h3 className="text-sm font-bold tracking-tight">
-                Permanent Address
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Bangalore,
-              </p>
-            </div>
-          </div>
+
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell colSpan={11}><h1 className="text-lg font-semibold md:text-lg">Performance Report</h1></TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[20px]">#</TableHead>
+                <TableHead>Subject Title</TableHead>
+                <TableHead>Code</TableHead>
+                <TableHead>Test I</TableHead>
+                <TableHead>Test II</TableHead>
+                <TableHead>Test III</TableHead>
+                <TableHead>Test Avg</TableHead>
+                <TableHead>LA-1</TableHead>
+                <TableHead>LA-2</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Remarks</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {invoices.map((invoice) => (
+                <TableRow key={invoice.invoice}>
+                  <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                  <TableCell>{invoice.subjectTitle}</TableCell>
+                  <TableCell >{invoice.subjectCode}</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell>100</TableCell>
+                  <TableCell>20</TableCell>
+                  <TableCell>30</TableCell>
+                  <TableCell>120</TableCell>
+                  <TableCell>test rem.</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell colSpan={11}><h1 className="text-lg font-semibold md:text-lg">Performance in SEE Exam</h1></TableCell>
+              </TableRow>
+              
+            </TableHeader>
+            <TableHeader>
+              <TableRow>
+                <TableHead rowSpan={2} className="w-[20px]">#</TableHead>
+                <TableHead rowSpan={2}>Subject Title</TableHead>
+                <TableHead colSpan={4} className="text-center border-l-2 border-l-indigo-500" >First attempt</TableHead>
+                <TableHead colSpan={4} className="text-center border-l-2 border-l-indigo-500">Second attempt</TableHead>
+                <TableHead colSpan={4} className="text-center border-l-2 border-l-indigo-500">Third attempt</TableHead>
+                
+              </TableRow>
+              <TableRow>
+                
+                <TableHead className="text-center border-l-2 border-l-indigo-500">Extenal</TableHead>
+                <TableHead >Reevalation</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Pass / Fail</TableHead>
+                
+                <TableHead className="text-center border-l-2 border-l-indigo-500">Extenal</TableHead>
+                <TableHead>Reevalation</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Pass / Fail</TableHead>
+
+                <TableHead className="text-center border-l-2 border-l-indigo-500">Extenal</TableHead>
+                <TableHead>Reevalation</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Pass / Fail</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {invoices.map((invoice) => (
+                <TableRow key={invoice.invoice}>
+                  <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                  <TableCell>{invoice.subjectTitle}</TableCell>
+                  <TableCell className="text-center border-l-2 border-l-indigo-500" >{invoice.subjectCode}</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell>Test1</TableCell>
+                  <TableCell className="text-center border-l-2 border-l-indigo-500">100</TableCell>
+                  <TableCell>20</TableCell>
+                  <TableCell>30</TableCell>
+                  <TableCell>120</TableCell>
+                  <TableCell className="text-center border-l-2 border-l-indigo-500">test rem.</TableCell>
+                  <TableCell>test rem.</TableCell>
+                  <TableCell>test rem.</TableCell>
+                  <TableCell>test rem.</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+
+
+
+
+
         </div>
       </div>
 
